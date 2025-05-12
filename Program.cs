81cf2595269bae0503f72afe804e4ff00c71d890
chagay <chagaytoubin@gmail.com>
2025-05-12 15:36:17 +0300

    Completion of building design

--- Program.cs
+++ Program.cs
@@ -1,10 +1,105 @@
-﻿namespace Series_analyzer
+﻿using System.Drawing;
+
+namespace Series_analyzer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Menu(args);
+        }
+        static void Menu(string [] SeriesList)//Show to user the option and instructions
+        {
+
+
+            bool check_exit = true;
+            while (check_exit)
+            {
+                Colorprint("----------------------welcome-----------------------");
+                Console.WriteLine(
+                    "\n Select the action you want to perform" +
+                    "\n Press 1-- for Print the series in the order it was entered. " +
+                    "\n press 2--Print the series in the reversorder it was " +
+                    "\n Press 3--Print the series sort." +
+                    "\n Press 4--Print the max value in series " +
+                    "\n Press 5--Print the min value in series " +
+                    "\n Press 6--Print the average of the series  " +
+                    "\n Press 7--Print the some of the series ");
+                
+
+                
+
+                string chose = Console.ReadLine();
+                switch (chose)
+                {
+                    case "1":
+                        PrintInOrder(SeriesList);
+                        break;
+                    case "2":
+                        PrintReversOrder(SeriesList);
+                        break;
+                    case "3":
+                        PrintSortOrder(SeriesList);
+                        break;
+                    case "4":
+                        FinfwMaximum(SeriesList);
+                        break;
+                        case "5":
+                        FindMin(SeriesList);
+                        break;
+                        case "6":
+                        Average(SeriesList); 
+                        break;
+                    case "7":
+                        ShowSumOfElment(SeriesList);
+                            break;
+
+
+
+
+
+
+
+                }
+
+            }
+        }
+        static void Colorprint(string  sentence )        //Change color for emphasize the  sentence
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(sentence);
+            Console.ResetColor();
+
+        }
+        static void PrintInOrder(string[] sentence)     //1Print the series in the order it was entered
+        {
+            Console.WriteLine();
+        }
+        static void PrintReversOrder(string[] sentence) //2Print the series in the reversorder it was
+        {
+            Console.WriteLine();
+        }
+        static void PrintSortOrder(string[] sentence)  //3Print the series sort 
+        { }
+
+        static void FinfwMaximum(string[] sentence)     //4Print the max value in series
+        { }
+        static void FindMin(string[] sentence)        //5Print the min value in series
+        { }
+        static void Average(string[] sentence)                    //6Print the average of the series 
+        { }
+        static void FindNumberOfElement(string[] sentence)//
+        {
+
+        }
+        static void ShowSumOfElment(string[] sentence)//7Print the some of the series 
+        {
+            
         }
     }
 }
